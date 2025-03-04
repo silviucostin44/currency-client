863c2002313661630773d62ad822bf70a249a3d4
QXZ499B <SilviuCostin.Miron@partner.bmw.de>
2025-03-05 00:14:45 +0200

    Tests fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,25 +6,10 @@ import {render, screen} from "./config/testUtils";
 
 
 describe('App', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
 
     it('renders with loading', async () => {
         render(<App/>);
         expect(screen.getByRole('progressbar')).toBeInTheDocument();
-    });
-
-    it('renders with currencies', async () => {
-        jest.spyOn(api, 'get').mockResolvedValueOnce({data: []});
-        render(<App/>);
-
-        await screen.findByText('Currency converter');
-
-        expect(screen.getByText('Currency converter')).toBeInTheDocument();
-        expect(screen.getAllByText('from').length).toBe(2);
-        expect(screen.getAllByText('to').length).toBe(2);
-        expect(screen.getAllByRole('button').at(-1)).toBeDisabled();
     });
 
     it('renders with error', async () => {
@@ -37,3 +22,15 @@ describe('App', () => {
         expect(linkElement).toBeInTheDocument();
     });
 })
+
+it('renders with currencies', async () => {
+    jest.spyOn(api, 'get').mockResolvedValueOnce({data: []});
+    render(<App/>);
+
+    await screen.findByText('Currency converter');
+
+    expect(screen.getByText('Currency converter')).toBeInTheDocument();
+    expect(screen.getAllByText('from').length).toBe(2);
+    expect(screen.getAllByText('to').length).toBe(2);
+    expect(screen.getAllByRole('button').at(-1)).toBeDisabled();
+});
